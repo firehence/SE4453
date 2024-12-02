ebd9b999932f21e1a3dd01df58babe394627b553
firehence <aytunyuksek48@gmail.com>
2024-12-02 15:18:30 +0300

    SE4453

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,6 +1,10 @@
-# Python program to print Hello, World!
-def main():
-    print("Hello, World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def home():
+    return "Hello"
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
