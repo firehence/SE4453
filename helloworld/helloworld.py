05f48ee17230d79ba77eea2ed4cc179c9e1cd3b2
firehence <aytunyuksek48@gmail.com>
2024-12-04 19:25:06 +0300

    Update helloworld.py

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def home():
-    return "Hello"
+    return "Hello, Azure!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
